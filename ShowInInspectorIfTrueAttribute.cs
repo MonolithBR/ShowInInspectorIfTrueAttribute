cb16f4760f309f471383b779c61b101dcd6376f6
MonolithBR <52931413+MonolithBR@users.noreply.github.com>
2022-11-23 21:32:37 -0300

    Add files via upload

---
+++ ShowInInspectorIfTrueAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class ShowInInspectorIfTrueAttribute : PropertyAttribute
+{
+    public string conditionProperty;
+    public ShowInInspectorIfTrueAttribute(string conditionProperty) { this.conditionProperty = conditionProperty; }
+}
---
+++ ShowInInspectorIfTrueDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShowInInspectorIfTrueAttribute))]
+public class ShowInInspectorIfTrueDrawer : PropertyDrawer
+{
+    private bool m_ShowInInspector;
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return m_ShowInInspector ? base.GetPropertyHeight(property, label) : 0;
+    }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var condProp = property.serializedObject.FindProperty(((ShowInInspectorIfTrueAttribute)attribute).conditionProperty);
+        m_ShowInInspector = condProp.boolValue;
+        if (m_ShowInInspector) 
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
